df705916cf2c18469c08eb258fbbf239a4047f09
Gabriel <gabrielhenrique_3l@outlook.com>
2020-04-08 17:03:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
+import TwilioVideo, { RoomNameCharsInvalidError } from 'twilio-video'
+
+import api from './services/api'
+
+export default function App() {
+  const [token, setToken] = useState('')
+  useEffect(() => {
+    async function getToken(){
+      const response = await api.get('auth');
+      setToken(response.data.token)
+    }
+    getToken();
+  },[])
+
+  async function connectVideo(){
+    const tracks = TwilioVideo.createLocalTracks({
+      audio: true,
+      video: {width: 640}
+    })
+
+    const room = await TwilioVideo.connect(token,{
+      name: 'Teste-video-room',
+      audio:true,
+      video: {width: 640},
+      tracks
+    })
+    
+    room.localParticipant.tracks.forEach(attachMedia);
+
+    room.participants.forEach(participant => {
+      participant.tracks.forEach(attachMedia)
+    })
+
+    room.on('participantConnected', participant => {
+      participant.tracks.forEach(attachMedia);
+      participant.on('trackSubscribed', track => {
+        const el = document.getElementById('#media-container')
+        el.appendChild(track.attach())
+      })
+    })
+  }
+
+  function attachMedia(publication){
+    console.log(publication)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {token && (
+      <button type="button" onClick={connectVideo}>CONECTAR</button>
+    )}
+    <div id="media-container">
+
     </div>
+    </>
   );
 }
-
-export default App;
